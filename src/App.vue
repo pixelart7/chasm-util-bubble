49d32246b9a129d8bbfb4bb8dd853c1ee5601e59
Chaiyapat Tantiworachot <chaiyapat@pix7.me>
2020-08-16 22:50:57 +0700

    filter blank lines

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 .app.p-4
   .flex
     .flex-1.px-2.py-2
-      textarea.border.border-blue-500.w-full(v-model="textInput" rows="20")
+      textarea.p-2.border.border-blue-500.w-full(v-model="textInput" rows="20")
     .flex-1.px-2
       Bubble.my-2(v-for="t in texts" :value="t")
   //- input.mt-4.rounded.border.border-gray-300.bg-gray-100(type="text" v-model="text")
@@ -19,10 +19,12 @@ const App = defineComponent({
   },
   setup() {
     const textInput = ref(`Which reminds me: He's sent me to find you.
+
 Your orders are to report to Commander Silxer's office at once.
+
 Maybe then some new customers will come along and try my brews.`);
 
-    const texts = computed(() => textInput.value.replace(/\r/g, '').split(/\n/));
+    const texts = computed(() => textInput.value.replace(/\r/g, '').split(/\n/).filter((t) => t !== ''));
 
     return {
       textInput,
